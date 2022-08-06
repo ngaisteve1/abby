c56a98da214260e9ed0f6fb95514e256d63e32d6
Steve Ngai <cheeweng.ngai@affinhwang.com>
2022-08-06 21:46:43 +0800

    .

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
   $('#tbl').DataTable(
     {
       order: [[0, 'desc']],
-      paging: false,
-      info: false
+      paging: true,
+      info: true
     }
   );
-debugger
+
+  showHideColumn(2, false);
+
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
@@ -29,4 +31,15 @@ function dateAgo(date) {
   var diffDate = new Date(new Date() - startDate);
   return ((diffDate.toISOString().slice(0, 4) - 1970) + "Y " +
       diffDate.getMonth() + "M " + (diffDate.getDate()-1) + "D");
+}
+
+function showHideColumn(col_no, do_show) {
+  var rows = document.getElementById('tbl').rows;
+
+  for (var row = 0; row < rows.length; row++) {
+      var cols = rows[row].cells;
+      if (col_no >= 0 && col_no < cols.length) {
+          cols[col_no].style.display = do_show ? '' : 'none';
+      }
+  }
 }
